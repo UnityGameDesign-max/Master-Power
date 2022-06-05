923c088e118e188b02d0915087e0f767b0d003f6
Tumelo <tumie201511@gmail.com>
2022-06-06 01:14:59 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
+import {NavigationBar} from './components/NavigationBar';
+import {Main} from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      hj
+      <NavigationBar />
+      <Main />
     </div>
   );
 }
