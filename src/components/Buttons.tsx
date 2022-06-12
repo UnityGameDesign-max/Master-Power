76232dc87c180b36c07bfe5437ca5b6c12eee4c5
Tumelo <tumie201511@gmail.com>
2022-06-12 04:10:10 +0200

    updated project files

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -4,12 +4,12 @@ import '../styles/Button.css';
 const Buttons = () => {
   return (
     <section className='buttons'>
-        <p>LEAD OR LAG</p>
-        <button className="option-btn lead">LEAD</button>
-        <button className="option-btn manual-auto">MANUAL</button>
-        <p>MODE</p>
+      <p>LEAD OR LAG</p>
+      <button className="option-btn lead">LEAD</button>
+      <button className="option-btn manual-auto">MANUAL</button>
+      <p>MODE</p>
     </section>
   )
 }
 
-export default Buttons
+export default Buttons;
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Layout, Avatar} from 'antd';
+import {Typography, Menu} from 'antd';
 
-export const NavigationBar = () => {
+const {Header} = Layout;
+const { Title } = Typography;
+
+export const NavigationBar : React.FC = () => {
   return (
-    <nav>NavigationBar</nav>
+    <nav>
+      <Layout>
+        <Header>
+          <Title style={{color: 'white'}} level={3}>Master Power PROJECT</Title>
+          <Menu defaultSelectedKeys={['Dashboard']} mode="inline">
+            <Menu.Item key='Dashboard'>
+              Main
+            </Menu.Item>
+            <Menu.Item key='Dashboard'>
+              Setting
+            </Menu.Item>
+          </Menu>
+        </Header>
+      </Layout>
+    </nav>
   )
 }
